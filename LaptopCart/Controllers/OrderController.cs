5cbc305a6f032e7698b4cd21adb4214ddb39fbea
SatwikaPapagani <papaganisatwika@gmail.com>
2026-06-12 11:29:11 +0530

    Step2_DI_Ex1
    Creating a Interface for Payment Service
    Creataing a folder for Services to implement Interface to class Services like Stripe, Paypal and Razor
    Calling the PaymentService using Dependency Injection
    Given an example for Dependency Injection - old way vs using dependency injection and advantages of DI

---
+++ LaptopCart/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using LaptopCart.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaptopCart.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IPaymentService _paymentSerive;
+        public OrderController(IPaymentService paymentService)
+        {
+            _paymentSerive = paymentService;
+        }
+
+        public IActionResult Index()
+        {
+            /* IPaymentService payment = new StripeService();
+             string result = payment.pay(1000);
+            */
+            //Old way beacuse we will be having multiple controllers(like orders, subscription, Donation etc) to call this serive or method and if in sometime the service is getting changed from on other(like Stripe to Paypall)
+            //then you must changes in all the controllers where this service(Stripe) is being used. Instead you can use Dependency injection concept here.
+
+            string result = _paymentSerive.pay(1000);
+            
+            return View();
+        }
+    }
+}
---
+++ LaptopCart/Services/Paypal.cs
@@ -0,0 +1,12 @@
+﻿namespace LaptopCart.Services
+{
+    public class Paypal : IPaymentService
+    {
+        public string pay(decimal amount)
+        {
+            return $"Paid {amount} using Paypal";
+        }
+
+
+    }
+}
